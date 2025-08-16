b7a726f1a5e44f8e05863cb8b53df5e8868752df
Andre Herman <amelco.herman@gmail.com>
2025-08-16 19:39:02 -0300

    experimentacao com winforms

--- Core/Models/ClienteModel.cs
+++ Core/Models/ClienteModel.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace Core.Models
 {
     public class ClienteModel
@@ -5,5 +7,15 @@ namespace Core.Models
         public string Nome { get; set; } = "";
         public int Idade { get; set; }
         // public decimal Salario { get; set; }
+        
+        public static ClienteModel? JsonToModel(string json)
+        {
+            return JsonConvert.DeserializeObject<ClienteModel>(json);
+        }
+
+        public static List<ClienteModel>? JsonToListModel(string json)
+        {
+            return JsonConvert.DeserializeObject<List<ClienteModel>>(json);
+        }
     }
 }
